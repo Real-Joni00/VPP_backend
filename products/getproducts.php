391bf2d1df028f1d10b8c59b241e646ec89b3fec
Joni Siitonen <n0sijo00@students.oamk.fi>
2022-11-15 19:49:48 +0200

    tuotteet taulu ja getproducts joka melkein toimii

---
+++ products/getproducts.php
@@ -0,0 +1,28 @@
+<?php
+require_once '../inc/functions.php';
+require_once '../inc/headers.php';
+
+$uri = parse_url(filter_input(INPUT_SERVER,'PATH_INFO'),PHP_URL_PATH);
+$parameters = explode('/',$uri);  //Jotain tässä on vikana lol. Koitan selvittää joskus paremmalla ajalla :)
+$category_id = $parameters[1];
+
+try {
+    $db = openDb();
+    $sql = "select * from kategoria where id = $category_id";
+    $query = $db->query($sql);
+    $category = $query->fetch(PDO::FETCH_ASSOC);
+
+    $sql = "select * from tuotteet where kategoria_id = $category_id";
+    $query = $db->query($sql);
+    $products = $query->fetchAll(PDO::FETCH_ASSOC);
+
+    header('HTTP/1.1 200 OK');
+    echo json_encode(array(
+        "kategoria" => $category['nimi'],
+        "tuotteet" => $tuotteet
+    ));
+}
+
+catch (PDOException $pdoex) {
+    returnError($pdoex);
+}
--- webshop.sql
+++ webshop.sql
@@ -9,3 +9,17 @@ create table tuotekategoriat (
 
 insert into tuotekategoriat (nimi) values ('grafiikkakortit');
 insert into tuotekategoriat (nimi) values ('prosessorit');
+
+create table tuotteet (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nimi VARCHAR(100) NOT NULL,
+    hinta DOUBLE (10,2) NOT NULL,
+    kuva VARCHAR(50),
+    kategoria_id INT NOT NULL,
+    index kategoria_id(kategoria_id),
+    FOREIGN KEY (kategoria_id) REFERENCES tuotekategoriat(id)
+    ON DELETE RESTRICT
+);
+
+insert into tuotteet (nimi,hinta,kategoria_id) values ('Testi tuote 1', 100,1);
+insert into tuotteet (nimi,hinta,kategoria_id) values ('Testi tuote 2', 200,1);
