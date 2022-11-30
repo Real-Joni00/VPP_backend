ef6e4d05632990e1e7fb63046d1901805c39172d
Joni Siitonen <n0sijo00@students.oamk.fi>
2022-11-30 18:25:46 +0200

    tuotekategorioiden ja tuotteiden ylläpito

---
+++ products/manageproducts.php
@@ -0,0 +1,18 @@
+<?php
+require_once '../inc/functions.php';
+require_once '../inc/headers.php';
+
+$input = json_decode(file_get_contents('php://input'));
+$name = filter_var($input->name,FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$price = filter_var($input->price,FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$category_id = filter_var($input->categoryid,FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+try {
+    $db = openDb();
+    $sql = "insert into tuotteet (nimi, hinta, kuva, kategoria_id) values ('$name', '$price', NULL, '$category_id')";
+    executeInsert($db, $sql);
+    $data = array('id' => $db->lastInsertId(), 'nimi' => $name, 'hinta' => $price, 'image' => NULL);
+    print json_encode($data);
+}catch (PDOException $pdoex) {
+    returnError($pdoex);
+}
